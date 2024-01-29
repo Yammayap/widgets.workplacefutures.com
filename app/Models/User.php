b5d8e7439183a3bdac14cf3c9492950dc62674bf
Jim Leeder <jim@yammayap.com>
2024-01-29 17:30:00 +0000

    Migration file for users and model file started

---
+++ app/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+    use HasUuid;
+}
