99323a518509014d63cf202bb60dce3aedae2613
Jim Leeder <jim@yammayap.com>
2024-02-02 10:54:19 +0000

    Build first text input component with example data

--- resources/views/web/space-calculator/inputs.blade.php
+++ resources/views/web/space-calculator/inputs.blade.php
@@ -2,6 +2,10 @@
 
 @section('page')
 
-    ...
+    <h1>Space Calculator Inputs</h1>
+
+    <p>Lorem ipsum dolor sit amet. This page is a WIP but demonstrates inputs and selects as view components below.</p>
+
+    <x-inputs.text name="test" label="This is a test" type="email" value="lorem" placeholder="ipsum" :required="true" :disableLabel="true" :classes="['class1', 'class2', 'flex']" />
 
 @endsection
