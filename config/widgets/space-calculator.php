3c2e2efca776ee144cc40103ca4dfa596434e950
Jim Leeder <jim@yammayap.com>
2024-02-07 15:23:16 +0000

    Empty array with all required keys

--- config/widgets/space-calculator.php
+++ config/widgets/space-calculator.php
@@ -4,4 +4,127 @@ use App\Enums\Widget;
 
 return [
     'widget' => Widget::SPACE_CALCULATOR,
+
+    'workstyle-parameters' => [
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::SERVICE_DELIVERY->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::CREATIVE->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::FINANCIAL->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::GENERAL->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::TECHNOLOGY->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::LAW->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::PROCESS_DRIVEN->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+        \App\Enums\Widgets\SpaceCalculator\Workstyle::PUBLIC_SECTOR->value => [
+            'hybrid-working' => [
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::OFFICE->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::ONE_DAY->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::TWO_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::THREE_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::FOUR_DAYS->value => '',
+                \App\Enums\Widgets\SpaceCalculator\HybridWorking::HOME->value => '',
+            ],
+            'workstations' => [
+                'private-offices' => '',
+                'use-of-touchdown' => '',
+            ],
+            'area-adjuster' => '',
+        ],
+    ],
 ];
