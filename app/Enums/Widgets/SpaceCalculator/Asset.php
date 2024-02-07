3f77538facbac8d17473ace16b6b409ad22927c0
Jim Leeder <jim@yammayap.com>
2024-02-07 10:05:47 +0000

    Front of house assets added to enum

--- app/Enums/Widgets/SpaceCalculator/Asset.php
+++ app/Enums/Widgets/SpaceCalculator/Asset.php
@@ -28,6 +28,18 @@ enum Asset: string implements HasLabel
     case GAMES_AREA = 'games-area';
     case GAMES_ROOM = 'games-room';
     case BREAKOUT_SEATS_OF_VARIOUS_TYPES = 'breakout-seats-of-various-types';
+    case RECEPTION_DESK_POSITIONS = 'reception-desk-positions';
+    case RECEPTION_WAITING_AREA_SEATS = 'reception-waiting-area-seats';
+    case RECEPTION_STORAGE = 'reception-storage';
+    case FURNITURE_STORE = 'furniture-store';
+    case FRONT_OF_HOUSE_KITCHEN = 'front-of-house-kitchen';
+    case TWO_PERSON_INTERVIEW_ROOM = 'two-person-interview-room';
+    case FOUR_PERSON_CONFERENCE_ROOM = 'four-person-conference-room';
+    case EIGHT_PERSON_CONFERENCE_ROOM = 'eight-person-conference-room';
+    case TWELVE_PERSON_CONFERENCE_ROOM = 'twelve-person-conference-room';
+    case SIXTEEN_PERSON_CONFERENCE_ROOM = 'sixteen-person-conference-room';
+    case TWENTY_PERSON_CONFERENCE_ROOM = 'twenty-person-conference-room';
+    case FIFTY_PERSON_CONFERENCE_ROOM = 'fifty-person-conference-room';
 
     /**
      * @return string
@@ -57,6 +69,18 @@ enum Asset: string implements HasLabel
             self::GAMES_AREA => 'Games Area',
             self::GAMES_ROOM => 'Games Room',
             self::BREAKOUT_SEATS_OF_VARIOUS_TYPES => 'Breakout Seats of Various Types',
+            self::RECEPTION_DESK_POSITIONS => 'Reception Desk Positions',
+            self::RECEPTION_WAITING_AREA_SEATS => 'Reception Waiting Area Seats',
+            self::RECEPTION_STORAGE => 'Reception Storage',
+            self::FURNITURE_STORE => 'Furniture Store',
+            self::FRONT_OF_HOUSE_KITCHEN => 'Front of House Kitchen',
+            self::TWO_PERSON_INTERVIEW_ROOM => 'Two Person Interview Room',
+            self::FOUR_PERSON_CONFERENCE_ROOM => 'Four Person Conference Room',
+            self::EIGHT_PERSON_CONFERENCE_ROOM => 'Eight Person Conference Room',
+            self::TWELVE_PERSON_CONFERENCE_ROOM => 'Twelve Person Conference Room',
+            self::SIXTEEN_PERSON_CONFERENCE_ROOM => 'Sixteen Person Conference Room',
+            self::TWENTY_PERSON_CONFERENCE_ROOM => 'Twenty Person Conference Room',
+            self::FIFTY_PERSON_CONFERENCE_ROOM => 'Fifty Person Conference Room',
         };
     }
 
@@ -85,6 +109,18 @@ enum Asset: string implements HasLabel
             self::GAMES_AREA => 'Open or semi-open games area for pool, table tennis etc',
             self::GAMES_ROOM => 'Enclosed room for electronic games',
             self::BREAKOUT_SEATS_OF_VARIOUS_TYPES => 'General breakout areas with a mixture of seats and tables',
+            self::RECEPTION_DESK_POSITIONS => 'Reception desk with a variable number of receptionists',
+            self::RECEPTION_WAITING_AREA_SEATS => 'Waiting area arranged in clusters',
+            self::RECEPTION_STORAGE => 'Room for post, parcels, coats and luggage',
+            self::FURNITURE_STORE => 'Store room for reconfigurable spaces such as seminar rooms',
+            self::FRONT_OF_HOUSE_KITCHEN => 'Food and beverage preparation area',
+            self::TWO_PERSON_INTERVIEW_ROOM => '2 person interview room for vendors, recruits etc',
+            self::FOUR_PERSON_CONFERENCE_ROOM => '4 person front of house meeting room',
+            self::EIGHT_PERSON_CONFERENCE_ROOM => '8 person front of house meeting room',
+            self::TWELVE_PERSON_CONFERENCE_ROOM => '12 person front of house meeting room',
+            self::SIXTEEN_PERSON_CONFERENCE_ROOM => '16 person front of house meeting room',
+            self::TWENTY_PERSON_CONFERENCE_ROOM => '20 person front of house meeting room',
+            self::FIFTY_PERSON_CONFERENCE_ROOM => '50 person front of house meeting room',
         };
     }
 
@@ -102,6 +138,11 @@ enum Asset: string implements HasLabel
             self::SERVERY, self::CATERING_KITCHEN_AND_STORES, self::CANTEEN_SEATING_SPACE, self::TEAPOINT,
             self::KITCHEN_AND_BAR, self::GAMES_AREA, self::GAMES_ROOM,
             self::BREAKOUT_SEATS_OF_VARIOUS_TYPES => AreaType::CONGREGATION_SPACE,
+            self::RECEPTION_DESK_POSITIONS, self::RECEPTION_WAITING_AREA_SEATS, self::RECEPTION_STORAGE,
+            self::FURNITURE_STORE, self::FRONT_OF_HOUSE_KITCHEN, self::TWO_PERSON_INTERVIEW_ROOM,
+            self::FOUR_PERSON_CONFERENCE_ROOM, self::EIGHT_PERSON_CONFERENCE_ROOM, self::TWELVE_PERSON_CONFERENCE_ROOM,
+            self::SIXTEEN_PERSON_CONFERENCE_ROOM, self::TWENTY_PERSON_CONFERENCE_ROOM,
+            self::FIFTY_PERSON_CONFERENCE_ROOM => AreaType::FRONT_OF_HOUSE,
         };
     }
 
@@ -111,7 +152,9 @@ enum Asset: string implements HasLabel
     public function capacityType(): CapacityType|null
     {
         return match ($this) {
-            self::SERVERY, self::CATERING_KITCHEN_AND_STORES, self::TEAPOINT, self::KITCHEN_AND_BAR => null,
+            self::SERVERY, self::CATERING_KITCHEN_AND_STORES, self::TEAPOINT, self::KITCHEN_AND_BAR,
+            self::RECEPTION_WAITING_AREA_SEATS, self::RECEPTION_STORAGE, self::FURNITURE_STORE,
+            self::FRONT_OF_HOUSE_KITCHEN => null,
             self::PHONE_BOOTH, self::OPEN_BOOTH, self::FOCUS_BOOTH,
             self::CHILL_QUIET_SPACE => CapacityType::FOCUS_SPACE,
             self::SCRUM_SPACE, self::OPEN_COLLABORATION_TOUCHDOWN, self::MEETING_BOOTH, self::WORKSHOP,
@@ -120,6 +163,10 @@ enum Asset: string implements HasLabel
             self::TWENTY_PERSON_MEETING_ROOM => CapacityType::TEAM_MEETING,
             self::CANTEEN_SEATING_SPACE, self::BREAKOUT_SEATS_OF_VARIOUS_TYPES => CapacityType::BREAKOUT,
             self::GAMES_AREA, self::GAMES_ROOM => CapacityType::RECREATION,
+            self::RECEPTION_DESK_POSITIONS => CapacityType::LONG_DWELL_WORKSTATION,
+            self::TWO_PERSON_INTERVIEW_ROOM, self::FOUR_PERSON_CONFERENCE_ROOM, self::EIGHT_PERSON_CONFERENCE_ROOM,
+            self::TWELVE_PERSON_CONFERENCE_ROOM, self::SIXTEEN_PERSON_CONFERENCE_ROOM,
+            self::TWENTY_PERSON_CONFERENCE_ROOM, self::FIFTY_PERSON_CONFERENCE_ROOM => CapacityType::FRONT_OF_HOUSE
         };
     }
 }
