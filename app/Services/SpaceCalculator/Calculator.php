823e5ada933b2ff4f377ff67f9da863ed0e0d9cb
Jim Leeder <jim@yammayap.com>
2024-02-13 10:49:42 +0000

    Started calculations on workstations sheet

--- app/Services/SpaceCalculator/Calculator.php
+++ app/Services/SpaceCalculator/Calculator.php
@@ -2,9 +2,11 @@
 
 namespace App\Services\SpaceCalculator;
 
+use Illuminate\Support\Arr;
+use Mattiasgeniar\Percentage\Percentage;
+
 class Calculator
 {
-   /** @phpstan-ignore-next-line */ // todo: set up config vars later and remove phpstan comment
     public function __construct(private readonly Config $config)
     {
         //
@@ -16,8 +18,32 @@ class Calculator
      */
     public function calculate(Inputs $inputs): Output
     {
-        // adding classes with empty results for now for Larastan and tests while we are only doing the structure
-        // todo: real calculations!
+        // step one: calculations here - workstations sheet
+
+        $peopleWorkingPlusGrowth = round(
+            $inputs->totalPeople + Percentage::of($inputs->growthPercentage, $inputs->totalPeople)
+        );
+
+        // todo: discuss - not sure if this needs to be rounded up here or not? (I don't think so?)
+        $percentageToAccommodate = Arr::get(
+            Arr::get(
+                $this->config->workstyleParameters,
+                $inputs->workstyle->value
+            ),
+            'hybrid-working.' . $inputs->hybridWorking->value
+        );
+
+        $undiversifiedAllocation = round(Percentage::of($inputs->deskPercentage, $peopleWorkingPlusGrowth));
+
+        // todo: discuss - should we round up $percentageToAccommodate here?
+        $diversifiedAllocation = round(
+            Percentage::of(
+                $percentageToAccommodate,
+                $peopleWorkingPlusGrowth - $undiversifiedAllocation
+            )
+        );
+
+        // end of calculations - empty outputs returned below
 
         $areaSize = new OutputAreaSize(0, 0, 0, 0, 0, 0);
         $assets = collect();
