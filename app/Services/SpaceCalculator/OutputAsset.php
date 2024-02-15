7dd8473b18a8ec04c7ebebf7a5792592e1792a25
Jim Leeder <jim@yammayap.com>
2024-02-15 11:05:43 +0000

    Add the three workstation types to the assets output collection

--- app/Services/SpaceCalculator/OutputAsset.php
+++ app/Services/SpaceCalculator/OutputAsset.php
@@ -3,15 +3,16 @@
 namespace App\Services\SpaceCalculator;
 
 use App\Enums\Widgets\SpaceCalculator\Asset;
+use App\Enums\Widgets\SpaceCalculator\WorkstationType;
 
 readonly class OutputAsset
 {
     /**
-     * @param Asset $asset
+     * @param Asset|WorkstationType $asset
      * @param int $quantity
      */
     public function __construct(
-        public Asset $asset,
+        public Asset|WorkstationType $asset,
         public int $quantity,
     ) {
         //
