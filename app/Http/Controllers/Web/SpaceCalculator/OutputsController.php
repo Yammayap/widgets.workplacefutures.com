edbe23d34a987903efbd7aeb361f437a119989ff
Jim Leeder <jim@yammayap.com>
2024-02-16 13:43:35 +0000

    New has_completed_profile column for users

--- app/Http/Controllers/Web/SpaceCalculator/OutputsController.php
+++ app/Http/Controllers/Web/SpaceCalculator/OutputsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web\SpaceCalculator;
 use App\Http\Controllers\WebController;
 use App\Http\Requests\Web\SpaceCalculator\Summary\PostIndexRequest;
 use App\Models\SpaceCalculatorInput;
+use App\Models\User;
 use App\Services\SpaceCalculator\Calculator;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
@@ -37,6 +38,28 @@ class OutputsController extends WebController
         PostIndexRequest $request,
         SpaceCalculatorInput $spaceCalculatorInput
     ): RedirectResponse {
-        dd('here', $request->all());
+
+        $user = User::query()->where('email', $request->input('email'))->first();
+
+        if ($user && $user->has_completed_profile) {
+            /*
+             * 1. run magic link action
+             * 2. return redirect to new /auth/sent page telling user to click link in the email
+             */
+            dd('WIP 1');
+        }
+
+        if (!$user) {
+            // create new user here and put into $user var - new action
+            dd('WIP 2');
+        }
+
+        /*
+         * more to do here
+         * 1. Update the enquiry (get via inputs) user id - new action
+         * 2. Send the user the summary notification
+         */
+
+        return redirect(route('web.space-calculator.outputs.index', $spaceCalculatorInput));
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,12 +26,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name'       => $this->faker->firstName,
-            'last_name'        => $this->faker->lastName,
-            'email'            => $this->faker->unique()->safeEmail,
-            'company_name'     => null,
-            'phone'            => null,
-            'marketing_opt_in' => false,
+            'first_name'            => $this->faker->firstName,
+            'last_name'             => $this->faker->lastName,
+            'email'                 => $this->faker->unique()->safeEmail,
+            'company_name'          => null,
+            'phone'                 => null,
+            'marketing_opt_in'      => false,
+            'has_completed_profile' => false,
         ];
     }
 
