e224df8afd845ce385d389d77375587006f659c5
George Buckingham <george@georgebuckingham.com>
2024-02-19 08:35:30 +0000

    Formatting

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,34 @@ Route::get('/', [Web\HomeController::class, 'getIndex'])
     ->name('home.index');
 
 Route::group(['prefix' => 'space-calculator'], function (): void {
+
     Route::get('/', [Web\SpaceCalculator\LandingController::class, 'getIndex'])
         ->name('space-calculator.index');
 
     Route::group(['prefix' => 'inputs'], function (): void {
         Route::get('/', [Web\SpaceCalculator\InputsController::class, 'getIndex'])
             ->name('space-calculator.inputs.index');
+
         Route::post('/', [Web\SpaceCalculator\InputsController::class, 'postIndex'])
             ->name('space-calculator.inputs.post');
     });
 
     Route::group(['prefix' => 'results/{spaceCalculatorInput}'], function (): void {
-        Route::group(
-            [
-                'prefix' => '/summary',
-                'middleware' => 'guard_space_calculator_output'],
-            function (): void {
-                Route::get('/', [Web\SpaceCalculator\OutputsController::class, 'getIndex'])
-                    ->name('space-calculator.outputs.index');
-                Route::post('/', [Web\SpaceCalculator\OutputsController::class, 'postIndex'])
-                    ->name('space-calculator.outputs.index.post');
-            }
-        );
+
+        Route::group([
+            'prefix' => '/summary',
+            'middleware' => 'guard_space_calculator_output'
+        ], function (): void {
+
+            Route::get('/', [Web\SpaceCalculator\OutputsController::class, 'getIndex'])
+                ->name('space-calculator.outputs.index');
+
+            Route::post('/', [Web\SpaceCalculator\OutputsController::class, 'postIndex'])
+                ->name('space-calculator.outputs.index.post');
+        });
 
         Route::group(['prefix' => 'detailed'], function (): void {
+
             Route::get('/', [Web\SpaceCalculator\OutputsController::class, 'getDetailed'])
                 ->name('space-calculator.outputs.detailed');
         });
@@ -51,8 +55,10 @@ Route::group([
 ], function (): void {
 
     Route::group(['prefix' => 'auth'], function (): void {
+
         Route::get('sent', [Web\AuthController::class, 'getSent'])
             ->name('auth.sent');
+
         Route::get('{magicLink}', [Web\AuthController::class, 'getMagicLink'])
             ->middleware('signed')
             ->name('auth.magic-link');
