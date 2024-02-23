2b1ce34a0deede166a8e405a20bbe59068cb87ff
Jim Leeder <jim@yammayap.com>
2024-02-23 13:39:15 +0000

    Fallback error pages for anything not covered in error views

---
+++ resources/views/errors/5xx.blade.php
@@ -0,0 +1,11 @@
+@extends('errors::layout')
+
+@section('title', $exception->getMessage())
+@section('code', $exception->getStatusCode())
+@section('message')
+    <p>It seems we're experiencing some technical problems.</p>
+    <p>Please try again later.</p>
+@endsection
+@section('actions')
+    <a href="{{ url()->previous(route('web.home.index')) }}" title="Go Back">Go Back</a>
+@endsection
