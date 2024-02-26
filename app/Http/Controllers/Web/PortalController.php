024c80e77d73c02b95f4d5b6986adcb389d0eab0
Jim Leeder <jim@yammayap.com>
2024-02-26 11:38:25 +0000

    Docblock and change meta title

--- app/Http/Controllers/Web/PortalController.php
+++ app/Http/Controllers/Web/PortalController.php
@@ -8,9 +8,12 @@ use Illuminate\View\View;
 
 class PortalController extends WebController
 {
+    /**
+     * @return View
+     */
     public function getIndex(): View
     {
-        $this->metaTitle('Portal');
+        $this->metaTitle('Welcome to your portal');
 
         $enquiries = Enquiry::query()
             ->where('user_id', $this->authUser()->id)
