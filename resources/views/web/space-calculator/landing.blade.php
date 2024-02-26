16dd8daab27f051bda0e6f17013e18266007103b
Jim Leeder <jim@yammayap.com>
2024-02-26 11:56:45 +0000

    Show a single link on SC landing page for auth users

--- resources/views/web/space-calculator/landing.blade.php
+++ resources/views/web/space-calculator/landing.blade.php
@@ -7,13 +7,18 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu quam, blandit eget sagittis nec, venenatis nec tellus. Cras mollis egestas molestie. Nam elit felis, facilisis eu enim quis, molestie iaculis nisi.</p>
 
     <div>
-        <div>
-            <a href="{{ route('web.auth.sign-in') }}" title="I've been here before">I've been here before</a>
-        </div>
-
-        <div>
-            <a href="{{ route('web.space-calculator.inputs.index') }}" title="I am new here">I am new here</a>
-        </div>
+        @guest {{-- todo: might need rearranging when designs are ready --}}
+            <div>
+                <a href="{{ route('web.auth.sign-in') }}" title="I've been here before">I've been here before</a>
+            </div>
+            <div>
+                <a href="{{ route('web.space-calculator.inputs.index') }}" title="I am new here">I am new here</a>
+            </div>
+        @else
+            <div>
+                <a href="{{ route('web.space-calculator.inputs.index') }}" title="Get Started">Get Started</a>
+            </div>
+        @endif
     </div>
 
 @endsection
